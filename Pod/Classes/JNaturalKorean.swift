04bb3b3147c5ffffbbba9cc0c1881578f31f6955
Ryan Yoo <yoobh17@gmail.com>
2016-03-04 01:01:33 +0900

    refactor : internal keyword로 내부에서 사용하는 함수, 상수를 숨김

--- Pod/Classes/JNaturalKorean.swift
+++ Pod/Classes/JNaturalKorean.swift
@@ -12,14 +12,14 @@ import Foundation
 
 public class JNaturalKorean {
     
-    static let baseCode = 44032
-    static let choSung = 588
-    static let jungSung = 28
+    internal static let baseCode = 44032
+    internal static let choSung = 588
+    internal static let jungSung = 28
     
-    static let choSungList = ["ㄱ", "ㄲ", "ㄴ", "ㄷ", "ㄸ", "ㄹ", "ㅁ", "ㅂ", "ㅃ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅉ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"]
-    static let jungSungList = ["ㅏ", "ㅐ", "ㅑ", "ㅒ", "ㅓ", "ㅔ", "ㅕ", "ㅖ", "ㅗ", "ㅘ", "ㅙ", "ㅚ", "ㅛ", "ㅜ", "ㅝ", "ㅞ", "ㅟ", "ㅠ", "ㅡ", "ㅢ", "ㅣ"]
-    static let jongSungList = [" ", "ㄱ", "ㄲ", "ㄳ", "ㄴ", "ㄵ", "ㄶ", "ㄷ", "ㄹ", "ㄺ", "ㄻ", "ㄼ", "ㄽ", "ㄾ", "ㄿ", "ㅀ", "ㅁ", "ㅂ", "ㅄ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"]
-    static let engCheckList = ["A", "a", "E", "e", "F", "f", "G", "g", "H", "h", "I", "i", "J", "j", "O", "o", "R", "r", "S", "s", "U", "u", "V", "v", "X", "x", "Y", "y", "Z", "z"]
+    internal static let choSungList = ["ㄱ", "ㄲ", "ㄴ", "ㄷ", "ㄸ", "ㄹ", "ㅁ", "ㅂ", "ㅃ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅉ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"]
+    internal static let jungSungList = ["ㅏ", "ㅐ", "ㅑ", "ㅒ", "ㅓ", "ㅔ", "ㅕ", "ㅖ", "ㅗ", "ㅘ", "ㅙ", "ㅚ", "ㅛ", "ㅜ", "ㅝ", "ㅞ", "ㅟ", "ㅠ", "ㅡ", "ㅢ", "ㅣ"]
+    internal static let jongSungList = [" ", "ㄱ", "ㄲ", "ㄳ", "ㄴ", "ㄵ", "ㄶ", "ㄷ", "ㄹ", "ㄺ", "ㄻ", "ㄼ", "ㄽ", "ㄾ", "ㄿ", "ㅀ", "ㅁ", "ㅂ", "ㅄ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"]
+    internal static let engCheckList = ["A", "a", "E", "e", "F", "f", "G", "g", "H", "h", "I", "i", "J", "j", "O", "o", "R", "r", "S", "s", "U", "u", "V", "v", "X", "x", "Y", "y", "Z", "z"]
     
     // MARK: - 이/가
     
@@ -85,22 +85,11 @@ public class JNaturalKorean {
     
 }
 
-// MARK: - Constants
-
-let baseCode = 44032
-let choSung = 588
-let jungSung = 28
-
-let choSungList = ["ㄱ", "ㄲ", "ㄴ", "ㄷ", "ㄸ", "ㄹ", "ㅁ", "ㅂ", "ㅃ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅉ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"]
-let jungSungList = ["ㅏ", "ㅐ", "ㅑ", "ㅒ", "ㅓ", "ㅔ", "ㅕ", "ㅖ", "ㅗ", "ㅘ", "ㅙ", "ㅚ", "ㅛ", "ㅜ", "ㅝ", "ㅞ", "ㅟ", "ㅠ", "ㅡ", "ㅢ", "ㅣ"]
-let jongSungList = [" ", "ㄱ", "ㄲ", "ㄳ", "ㄴ", "ㄵ", "ㄶ", "ㄷ", "ㄹ", "ㄺ", "ㄻ", "ㄼ", "ㄽ", "ㄾ", "ㄿ", "ㅀ", "ㅁ", "ㅂ", "ㅄ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"]
-let engCheckList = ["A", "a", "E", "e", "F", "f", "G", "g", "H", "h", "I", "i", "J", "j", "O", "o", "R", "r", "S", "s", "U", "u", "V", "v", "X", "x", "Y", "y", "Z", "z"]
-
 // MARK: - String+JNaturalKorean Internal Util for Hangul
 
 extension String {
     
-    var isHangul: Bool {
+    internal var isHangul: Bool {
         guard let lastUnicode = self.unicodeScalars.last else {
             return false
         }
@@ -108,36 +97,36 @@ extension String {
         return 44032 <= last && last <= 55199
     }
     
-    // TODO: Need Refactoring
-    var isThere종성: Bool {
-        let cBase = Int((String(self.characters.last!).unicodeScalars.first?.value)!) - baseCode
+    // TODO: Consider Refactoring
+    internal var isThere종성: Bool {
+        let cBase = Int((String(self.characters.last!).unicodeScalars.first?.value)!) - JNaturalKorean.baseCode
         
-        let cs = (cBase / choSung)
-        let jus = (cBase - (choSung * cs)) / jungSung
-        let jos = (cBase - (choSung * cs) - (jungSung * jus))
+        let cs = (cBase / JNaturalKorean.choSung)
+        let jus = (cBase - (JNaturalKorean.choSung * cs)) / JNaturalKorean.jungSung
+        let jos = (cBase - (JNaturalKorean.choSung * cs) - (JNaturalKorean.jungSung * jus))
         
-        jongSungList[jos]
+        JNaturalKorean.jongSungList[jos]
         
-        if jongSungList[jos] == " " {
+        if JNaturalKorean.jongSungList[jos] == " " {
             return false
         }
         
         return true
     }
     
-    // TODO: - Need Refactoring
-    var isㄹ종성: Bool {
-        let cBase = Int((String(self.characters.last!).unicodeScalars.first?.value)!) - baseCode
+    // TODO: - Consider Refactoring
+    internal var isㄹ종성: Bool {
+        let cBase = Int((String(self.characters.last!).unicodeScalars.first?.value)!) - JNaturalKorean.baseCode
         
-        let cs = cBase / choSung
-        let jus = (cBase - (choSung * cs)) / jungSung
-        let jos = (cBase - (choSung * cs) - (jungSung * jus))
+        let cs = cBase / JNaturalKorean.choSung
+        let jus = (cBase - (JNaturalKorean.choSung * cs)) / JNaturalKorean.jungSung
+        let jos = (cBase - (JNaturalKorean.choSung * cs) - (JNaturalKorean.jungSung * jus))
         
-        choSungList[cs]
-        jungSungList[jus]
-        jongSungList[jos]
+        JNaturalKorean.choSungList[cs]
+        JNaturalKorean.jungSungList[jus]
+        JNaturalKorean.jongSungList[jos]
         
-        if jongSungList[jos] == "ㄹ" {
+        if JNaturalKorean.jongSungList[jos] == "ㄹ" {
             return true
         }
         
@@ -148,7 +137,7 @@ extension String {
 
 // MARK: - String+JNaturalKorean Internal Util for English
 
-enum 영어받침: String {
+internal enum 영어받침: String {
     
     case NG = "NG"
     case LE = "LE"
@@ -164,7 +153,7 @@ enum 영어받침: String {
 
 extension String {
     
-    var isEnglish: Bool {
+    internal var isEnglish: Bool {
         guard let lastUnicode = self.unicodeScalars.last else {
             return false
         }
@@ -172,13 +161,13 @@ extension String {
         return (65 <= last && last <= 90) || (97 <= last && last <= 122)
     }
     
-    var lastTwoCharString: String {
+    internal var lastTwoCharString: String {
         return self.characters.suffix(2).map { char in return String(char) }.reduce("", combine: +)
     }
     
-    var isKindOf받침: Bool {
+    internal var isKindOf받침: Bool {
         guard let 영어받침 = 영어받침(rawValue: self.lastTwoCharString.uppercaseString) else {
-            return !engCheckList.contains(String(self.characters.last!))
+            return !JNaturalKorean.engCheckList.contains(String(self.characters.last!))
         }
         switch 영어받침 {
         case .NG, .LE, .ME:
@@ -188,8 +177,8 @@ extension String {
         }
     }
     
-    // TODO: - Need Refactoring
-    var isL: Bool {
+    // TODO: - Consider Refactoring
+    internal var isL: Bool {
         let value = Int((String(self.characters.last!).unicodeScalars.first?.value)!)
         if value == 76 || value == 108 {
             return true
