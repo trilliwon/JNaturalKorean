63714fae7083c612ddf4a2fd03ecdbb18cafebbe
Ryan Yoo <yoobh17@gmail.com>
2016-03-04 01:11:48 +0900

    improvement : String extension에 조사만 반환하는 프로퍼티 추가

--- Pod/Classes/String+JNaturalKorean.swift
+++ Pod/Classes/String+JNaturalKorean.swift
@@ -8,24 +8,53 @@
 
 import Foundation
 
-// MARK: - String+JNaturalKorean Public API
+// MARK: - String+JNaturalKorean Public Convenient API
 
 extension String {
+    
+    // MARK: - 이/가
     
     var 이_가: String {
         return JNaturalKorean.get_이_가_with(self)
     }
+    var 이_가_only: String {
+        return JNaturalKorean.get_이_가(self)
+    }
+    
+    // MARK: - 은/는
+    
     var 은_는: String {
         return JNaturalKorean.get_은_는_with(self)
     }
-    var 을_를: String {
-        return JNaturalKorean.get_을_를_with(self)
+    var 은_는_only: String {
+        return JNaturalKorean.get_은_는(self)
     }
+    
+    // MARK: - 을/를
+    
+    var 을_를: String {
+        return JNaturalKorean.get_을_를(self)
+    }
+    var 을_를_only: String {
+        return JNaturalKorean.get_을_를(self)
+    }
+    
+    // MARK: - 으로/로
+    
     var 으로_로: String {
         return JNaturalKorean.get_으로_로_with(self)
     }
+    var 으로_로_only: String {
+        return JNaturalKorean.get_으로_로(self)
+    }
+    
+    // MARK: - 아/야
+    
     var 아_야: String {
         return JNaturalKorean.get_아_야_with(self)
+    }
+    var 아_야_only: String {
+        return JNaturalKorean.get_아_야(self)
     }
     
 }
