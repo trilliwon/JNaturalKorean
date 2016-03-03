6eeadee398a47fe176852b25f7a92f06916988bd
Ryan Yoo <yoobh17@gmail.com>
2016-03-03 20:12:35 +0900

    refactor : 전체 라이브러리 구조 리펙토링

--- Pod/Classes/String+JNaturalKorean.swift
+++ Pod/Classes/String+JNaturalKorean.swift
@@ -8,22 +8,24 @@
 
 import Foundation
 
+// MARK: - String+JNaturalKorean Public API
+
 extension String {
     
     var 이_가: String {
-        return JNaturalKorean.instance.getWith주격조사_이_가(self)
+        return JNaturalKorean.get_이_가_with(self)
     }
     var 은_는: String {
-        return JNaturalKorean.instance.getWith보조사_은_는(self)
+        return JNaturalKorean.get_은_는_with(self)
     }
     var 을_를: String {
-        return JNaturalKorean.instance.getWith목적격조사_을_를(self)
+        return JNaturalKorean.get_을_를_with(self)
     }
     var 으로_로: String {
-        return JNaturalKorean.instance.getWith부사격조사_으로_로(self)
+        return JNaturalKorean.get_으로_로_with(self)
     }
     var 아_야: String {
-        return JNaturalKorean.instance.getWith호격조사_아_야(self)
+        return JNaturalKorean.get_아_야_with(self)
     }
     
 }
